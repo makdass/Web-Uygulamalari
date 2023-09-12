bf3a5f8fef2266d4860dbae9dace75bfd5ddf66f
Mustafa Akdas <makdas163@gmail.com>
2023-09-12 13:58:57 +0300

    görev tanimi atandi

---
+++ Screesns/Login.js
@@ -0,0 +1,13 @@
+/*login ekranında öncelikle kalın ve büyük fontla Hoşgeldiniz yaz 
+
+kullanıcıdan kullanıcı adı ve şifre girebileceği iki textinput oluştur giriş yap butonu ekle 
+
+googlela apple ile ya da facebook ile giriş yapmak isteyenler için bunların simgelerini yerleştir (çalışmasın ama orda simgeleri olsun)
+
+şifermi unuttum ve hesap oluştur butonlarını ekle 
+
+
+tasarım renklendirme sana kalmış 
+
+
+*/
